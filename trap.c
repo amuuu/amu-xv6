f02aaf104892ae1f04bf134f820f57cee6c94619
Amirreza Amouie <aamouie01@gmail.com>
2019-12-29 05:44:23 -0600

    update rtime and iotime in trap.c

--- trap.c
+++ trap.c
@@ -53,7 +53,16 @@ trap(struct trapframe *tf)
       ticks++;
       wakeup(&ticks);
       release(&tickslock);
+      
+      // Update rtime or iotime
+      if (myproc()) {
+        if (myproc()->state == RUNNING)
+          myproc()->rtime++;
+        else if (myproc()->state == SLEEPING)
+          myproc()->iotime++;
+      }
     }
+
     lapiceoi();
     break;
   case T_IRQ0 + IRQ_IDE:
